0ab12c482ae4ae18307b4bc52b195e206073a7ba
ana-lapas <anapaulalapas@gmail.com>
2023-05-09 22:20:20 -0400

    Feat: CSS de Payment/Ingresso/PresencialVsOnline

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,36 @@
+import { useState } from 'react';
+import { Container, Options, Option, Button } from '../../../components/Dashboard/Payments';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const [total, setTotal] = useState(0);
+  return (<>
+    <Container>
+      <h1> Ingressos e Pagamento</h1>
+      <h2>Primeiro, escolha a modalidade de ingresso</h2>
+      <Options>
+        <Option>
+          <p>Presencial</p>
+          <p className='price'> R$XXXX,00</p>
+        </Option>
+        <Option>
+          <p>Online</p>
+          <p className='price'> R$XXXX,00</p>
+        </Option>
+      </Options>
+      <h2>Ótimo! Agora escolha sua modalidade de hospedagem</h2>
+      <Options>
+        <Option>
+          <p>Sem hotel</p>
+          <p className='price'> R$XXXX,00</p>
+        </Option>
+        <Option>
+          <p>Com hotel</p>
+          <p className='price'> R$XXXX,00</p>
+        </Option>
+      </Options>
+      <h2>Fechado! O total ficou em ${total}. Agora é só confirmar!</h2>
+      
+      <Button><p>RESERVAR INGRESSO</p></Button>
+    </Container>
+  </>);
 }
