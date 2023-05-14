07a7daaf61b766736ae30b5981d5e29688c71611
ana-lapas <anapaulalapas@gmail.com>
2023-05-13 22:54:33 -0400

    Fix: values reservation

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -6,10 +6,10 @@ import { Container, Options, Option, Button, Row, Column, StyledTypography, Rese
 export default function Payment() {
   const navigate = useNavigate();
 
-  const [pricePr, setPricePr] = useState('X');
-  const [priceOn, setPriceOn] = useState('Y');
+  const [pricePr, setPricePr] = useState(150);
+  const [priceOn, setPriceOn] = useState(50);
   const [type, setType] = useState('');
-  const [includesHotel, setincludesHotel] = useState('');
+  const [includesHotel, setIncludesHotel] = useState('');
   const [total, setTotal] = useState(0);
 
   return (
@@ -22,11 +22,11 @@ export default function Payment() {
       <Row>
         <OptionBox onClick={() => setType('presencial')} style={{ 'background-color': type === 'presencial' ? '#FFEED2' : 'white' }}>
           <h1>Presencial</h1>
-          <h2>R${pricePr}</h2>
+          <h2 onClick={() => setTotal(pricePr)}>R${pricePr}</h2>
         </OptionBox>
         <OptionBox onClick={() => setType('online')} style={{ 'background-color': type === 'online' ? '#FFEED2' : 'white' }}>
           <h1>Online</h1>
-          <h2>R${priceOn}</h2>
+          <h2 onClick={() => setTotal(priceOn)}>R${priceOn}</h2>
         </OptionBox>
       </Row>
 
@@ -41,31 +41,33 @@ export default function Payment() {
         </Column>
       }
 
-      {type == 'presencial' ? 
+      {type == 'presencial' ?
         <>
           <StyledTypography variant="h6" style={{ 'color': '#8E8E8E', 'font-weight': '400', 'margin-top': '32px' }}>Ótimo! Agora escolha sua modalidade de hospedagem</StyledTypography>
           <Row>
-            <OptionBox onClick={() => setincludesHotel(false)} style={{ 'background-color': includesHotel === false ? '#FFEED2' : 'white' }}>
+            <OptionBox onClick={() => setIncludesHotel(false)} style={{ 'background-color': includesHotel === false ? '#FFEED2' : 'white' }}>
               <p>Sem hotel</p>
-              <p className='price'> R$XXXX,00</p>
+              <p className='price' onClick={() => setTotal(200)}> R$50,00</p>
             </OptionBox>
-            <OptionBox onClick={() => setincludesHotel(true)} style={{ 'background-color': includesHotel === true ? '#FFEED2' : 'white' }}>
+            <OptionBox onClick={() => setIncludesHotel(true)} style={{ 'background-color': includesHotel === true ? '#FFEED2' : 'white' }}>
               <p>Com hotel</p>
-              <p className='price'> R$XXXX,00</p>
+              <p className='price' onClick={() => setTotal(400)}> R$250,00</p>
             </OptionBox>
           </Row>
-        </> 
+        </>
         : ''
       }
 
-      {includesHotel !== '' ? 
+      {includesHotel !== '' ?
         <>
-          <StyledTypography variant="h6" style={{ 'color': '#8E8E8E', 'font-weight': '400', 'margin-top': '32px' }}>Fechado! O total ficou em ${total}. Agora é só confirmar!</StyledTypography>
+          <StyledTypography variant="h6" style={{ 'color': '#8E8E8E', 'font-weight': '400', 'margin-top': '32px' }}>
+            Fechado! O total ficou em <span style={{ 'font-weight': '500' }}>R$ {total}.</span> Agora é só confirmar:
+          </StyledTypography>
           <ReserveButton><p>RESERVAR INGRESSO</p></ReserveButton>
         </>
         : ''
       }
-      
+
     </>
   );
 }
