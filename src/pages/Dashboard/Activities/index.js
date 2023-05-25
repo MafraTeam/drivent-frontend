9bb592be8f03b54915037c278665848031f3999e
AnneKinder <annekhd98@gmail.com>
2023-05-25 19:36:39 -0300

    feat: title

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,16 @@
+import { useState } from 'react';
+import { SubTitles, Title } from '../../../components/Hotel/HotelStyle';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const [isPaid, setIsPaid] = useState(true);
+
+  return (
+    <>
+      {!isPaid ? '' : <>
+        <Title>Escolha de atividades</Title>
+        <SubTitles style={{ color: '#8e8e8e', 'margin-top': '20px', 'font-size': '20px' }}>Primeiro, filtre pelo dia do evento: </SubTitles>
+      </>
+      }
+    </>
+  );
 }
